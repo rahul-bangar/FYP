82d7a5d0a78e59744882e7611b747dd671adbd8e
rahul-bangar <bangarrahul.b@gmail.com>
2024-04-24 23:43:31 +0530

    explorer added

--- explorer/start.sh
+++ explorer/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd organizations/
+cd ~/fabric-samples/explorer/organizations
 rm -rf *
-cp -r ~/fabric-samples/test-network/organizations/   ~/explorer-copy/organizations
+cp -r ~/fabric-samples/test-network/organizations/   ~/fabric-samples/explorer/organizations
 cd peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore
 filename=$(ls)
 mv $filename priv_sk
-cd ~/explorer-copy
+cd ~/fabric-samples/explorer
 docker-compose up -d
