112682275341b592e89924d54f8b722ef303f006
rahul-bangar <bangarrahul.b@gmail.com>
2024-04-24 23:57:48 +0530

    added gitignore in explorer

--- explorer/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~/fabric-samples/explorer/organizations
-rm -rf *
-cp -r ~/fabric-samples/test-network/organizations/   ~/fabric-samples/explorer/organizations
-cd peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore
-filename=$(ls)
-mv $filename priv_sk
-cd ~/fabric-samples/explorer
-docker-compose up -d
